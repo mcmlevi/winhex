c0587f7db42c4a170a4b09585be2e9eb4c3ba2c3
levi <mcmlevi@outlook.com>
2023-05-24 20:50:37 +0200

    Part 1 of accepting UTF-8 view

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,18 @@ use clap::Parser;
 struct Args {
     /// The fileName to use in winhex
     file: std::path::PathBuf,
+
     /// the hex width
-    #[arg(default_value_t = 16)]
+    #[arg(long, default_value_t = 16)]
     width: u16,
 
     /// The number of rows to display
-    #[arg(default_value_t = 16)]
+    #[arg(long, default_value_t = 16)]
     height: u16,
+
+    /// Output the text as UTF-8
+    #[arg(long,default_value_t = false)]
+    utf8 : bool,
 }
 
 fn format_header(width : u16) {
@@ -30,17 +35,31 @@ fn format_header(width : u16) {
     println!("{header}\n");
 }
 
-fn format_row(offset : u64, slice : &[u8]) {
+fn format_row(offset : u64, slice : &[u8], is_utf8 : bool) {
     let mut row = String::new();
-    row += &format!("{:0>8}   ", offset.to_string());
-
-    for elem in slice {
-        row += &format!("{:02x} ", elem);
+    row += &format!("{:0>8x}   ", offset);
+    
+    let mut mutable_slice = slice.to_vec();
+    if is_utf8{
+        
+        for elem in mutable_slice.iter_mut() {
+            row += &format!("{:02x} ", elem);
+        }
     }
-
-    row += &String::from_utf8_lossy(&slice);
-
-    println!("{row}\n");
+    else {
+        for elem in mutable_slice.iter_mut() {
+            row += &format!("{:02x} ", elem);
+    
+            // Replace non printable characters to a . and all characters that are not normal ASCII.
+            if *elem < ' ' as u8 || *elem > '~' as u8 {
+                *elem = '.' as u8;
+            }
+        }
+    
+        row += &String::from_utf8_lossy(&mutable_slice);
+    
+        println!("{row}\n");
+    }
 }
 
 fn main() {
@@ -49,8 +68,8 @@ fn main() {
     match fs::read(args.file) {
         Ok(buffer) => {
             format_header(args.width);
-            for (i, slice) in buffer.windows(args.width as usize).enumerate() {
-                format_row(i as u64 * args.width as u64, slice);
+            for (i, slice) in buffer.windows(args.width as usize).step_by(args.width as usize).enumerate() {
+                format_row(i as u64 * args.width as u64, slice, args.utf8);
                 
                 //TODO this should prompt for more input possibly
                 if i >= args.height as usize {
